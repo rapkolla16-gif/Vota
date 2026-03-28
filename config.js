65cd37fdcc1245b229616dafe192634f13172844
rapkolla16-gif <rapkolla16@gmail.com>
2026-03-28 09:28:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rxhCmL6K#lfvEhWADmR-bD5x8w5Bez429y49zpjOCKLp_5enn3f0", 
+SESSION_ID: process.env.SESSION_ID || "XsYknKhJ#mcKqqNosv_JmmW6-d4m73l7TEd5nzSFtItAQktnxuow", 
 WORK_MODE: process.env.WORK_MODE || 'public',
 MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://free62:ranu123@cluster0.rxwlzad.mongodb.net/?appName=Cluster0', // මෙතනට URL එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/ZRXhhYxH/db1c9ed7-6513-49da-8105-f21c73583135.png",
