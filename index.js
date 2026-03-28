60f805bd125f2b53c58141462bf3f193148461ff
rapkolla16-gif <rapkolla16@gmail.com>
2026-03-28 09:53:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ if (mek.key?.remoteJid === 'status@broadcast') {
           text: randomEmoji,
           key: mek.key,
         }
-      });
+      }
 
       console.log(`[✓] Reacted to status of ${mek.key.participant} with ${randomEmoji}`);
     } catch (e) {
