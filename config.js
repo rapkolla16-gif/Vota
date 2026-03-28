7921cbc7fec989d7a5efde5495ddb3858e02bdce
rapkolla16-gif <rapkolla16@gmail.com>
2026-03-28 11:07:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ WORK_MODE: process.env.WORK_MODE || 'public',
 MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://free62:ranu123@cluster0.rxwlzad.mongodb.net/?appName=Cluster0', // මෙතනට URL එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/ZRXhhYxH/db1c9ed7-6513-49da-8105-f21c73583135.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VERTEX-MD Is Alive Now😍*",
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94783938367', // Replace with the owner's phone number
+OWNER_NUMBER: process.env.OWNER_NUMBER || '947839383678', // Replace with the owner's phone number
 AUTO_STATUS_SEEN: 'true',
 AUTO_STATUS_REACT: 'true',
 
