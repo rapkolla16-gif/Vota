cbdc63f8eb07a1390c98ee4e979b54bfda4fa241
rapkolla16-gif <rapkolla16@gmail.com>
2026-03-28 16:02:10 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -9,12 +9,12 @@ cmd({
     react: "🧬",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, reply}) => {
-    try{
+async(conn, mek, m, { from, quoted, reply, prefix }) => { // මෙතනට prefix එක එකතු කළා
+    try {
         const startTime = Date.now();
-        const ping = Date.now() - startTime; // Bot speed calculation
+        const ping = Date.now() - startTime; 
 
-        let aliveMsg = `╭───「 *VERTEX-MD IS ALIVE* 」───⊷
+        let aliveMsg = `╭───「 *VEXTER-MD IS ALIVE* 」───⊷
 │
 │ 👤 *User:* ${m.pushName}
 │ ⏳ *Uptime:* ${runtime(process.uptime())}
@@ -24,28 +24,28 @@ async(conn, mek, m,{from, quoted, reply}) => {
 │
 ├──────────────────────────⊷
 │
-│ *Created By Ranu*
-│ *Powered By VERTEX*
+│ *Created By Dexter*
+│ *Powered By Ranu Social Booster*
 │
 ╰──────────────────────────⊷
-> *Type .menu to see commands* 🧬`
+> *Type ${prefix}menu to see commands* 🧬`
 
         await conn.sendMessage(from, { 
             image: { url: config.ALIVE_IMG }, 
             caption: aliveMsg,
             contextInfo: {
                 externalAdReply: {
-                    title: "🧬 VERTEX-MD ONLINE",
+                    title: "🧬 VEXTER-MD ONLINE",
                     body: "Created By Dexter | Official Bot",
                     mediaType: 1,
                     thumbnailUrl: config.ALIVE_IMG,
-                    renderLargerThumbnail: false, // අපි කලින් කතා වුණ විදියට මේක false කළා ලස්සන වෙන්න
+                    renderLargerThumbnail: false,
                     sourceUrl: "https://wa.me/94783462955"
                 }
             }
         }, { quoted: mek });
 
-    } catch(e) {
+    } catch (e) {
         console.log(e)
         reply(`${e}`)
     }
